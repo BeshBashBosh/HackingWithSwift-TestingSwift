20ee0898fc889cded1e2e566466dea2e48c7ac2d
Benjamin Hall <b.e.s.hall@outlook.com>
2018-12-07 18:52:36 +0000

    Refactored TemperatureConverter tests further to use setUp() and tearDown() overrides.

--- First/FirstTests/TemperatiureConverterTests.swift
+++ First/FirstTests/TemperatiureConverterTests.swift
@@ -11,40 +11,44 @@ import XCTest
 
 class TemperatiureConverterTests: XCTestCase {
 
+    var sut: TemperatureConverter!
+    
     override func setUp() {
+        sut = TemperatureConverter()
+        
         // This will stop the testing as soon as a single test fails
         // Typically used in UI tests
         // continueAfterFailure = false
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
 //    func testFahrenheitToCelsius() {
 //        let sut = TemperatureConverter() // System - Under - Test
-//        
+//
 //        let input1 = 32.0
 //        let output1 = sut.convertToCelsius(fahrenheit: input1)
 //        XCTAssertEqual(output1, 0)
-//        
+//
 //        let input2 = 212.0
 //        let output2 = sut.convertToCelsius(fahrenheit: input2)
 //        XCTAssertEqual(output2, 100)
 //    }
     
     func test32FahrenheitIsZeroCelsius() {
-        let sut = TemperatureConverter() // Arrange
+        let input = 32.0 // Arrange
         
-        let output = sut.convertToCelsius(fahrenheit: 32.0) // Act
+        let output = sut.convertToCelsius(fahrenheit: input) // Act
         
         XCTAssertEqual(output, 0) // Assert
     }
     
     func test212FahrenheitIs100Celsius() {
-        let sut = TemperatureConverter()
+        let input = 212.0
         
-        let output = sut.convertToCelsius(fahrenheit: 212.0)
+        let output = sut.convertToCelsius(fahrenheit: input)
         
         XCTAssertEqual(output, 100)
     }
